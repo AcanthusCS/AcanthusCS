b5c8b16e9e2a7ab3f7cecfba2a9f63bc9be1df78
Jesse William Freeman <jessewilliamfreeman@gmail.com>
2019-01-13 17:03:33 -0500

    changing landing plus more cleanup

--- src/components/landing.js
+++ src/components/landing.js
@@ -13,6 +13,10 @@ class Landing extends React.Component {
       <div id="landing" className={styles.landing}>
         <div className={styles.landingContent}>
           <img src={logo} alt="logo" className={styles.logo}/>
+          <div className={styles.line + " dark-blue"}/>
+          <div className={styles.line + " light-blue"}/>
+          <div className={styles.line + " light-green"}/>
+          <div className={styles.line + " dark-green"}/>
           <h2 className={styles.landingText}>
             {this.props.data.text}
           </h2>
