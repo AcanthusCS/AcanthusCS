b0f4a57900f2e8b91094124f1263d0f0c86ac400
Jesse William Freeman <jessewilliamfreeman@gmail.com>
2019-02-05 16:10:21 -0500

    more seo edits

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import logo from "../images/logo2.png"
+import logo from "../images/icon2.png"
 
 import "./layout.scss"
 
@@ -25,31 +25,25 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      data.allDataYaml.edges.map( edge =>
-        <>
-          <Helmet
-            title={edge.node.meta.title}
-            meta={[
-              { name: 'description', content: edge.node.meta.description },
-              { name: 'keywords', content: edge.node.meta.keywords },
-            ]}
-          >
-            <meta charset="utf-8" />
-            <script async src="https://cdn.ampproject.org/v0.js" />
-            <meta property="og:title" content={edge.node.meta.title} />
-            <meta property="og:description" content={edge.node.meta.description} />
-            <meta property="og:image" content={logo} />
-            <meta property="og:url" content="https://acanthuscs.com" />
-            <meta name="twitter:card" content="summary_large_image" />
-            <link href="https://fonts.googleapis.com/css?family=Oswald:300|Raleway:400" rel="stylesheet"/>
-            <meta name="google-site-verification" content="i0-pVtkViJQuUt9OtyXJwqJ1ozlW1lt15weEMFTnc1k" />
-
-          </Helmet>
-          <body>
-            {children}
-          </body>
-        </>
-      )
+      <>
+        <Helmet
+          title={data.allDataYaml.edges[0].node.meta.title}
+          meta={[
+            { name: 'description', content: data.allDataYaml.edges[0].node.meta.description },
+            { name: 'keywords', content: data.allDataYaml.edges[0].node.meta.keywords },
+          ]}
+        >
+          <meta charset="utf-8" />
+          <meta property="og:title" content={data.allDataYaml.edges[0].node.meta.title} />
+          <meta property="og:description" content={data.allDataYaml.edges[0].node.meta.description} />
+          <meta property="og:image" content={logo} />
+          <meta property="og:url" content="https://acanthuscs.com" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <link href="https://fonts.googleapis.com/css?family=Oswald:300|Raleway:400" rel="stylesheet"/>
+          <meta name="google-site-verification" content="i0-pVtkViJQuUt9OtyXJwqJ1ozlW1lt15weEMFTnc1k" />
+        </Helmet>
+        {children}
+      </>
     )}
   />
 )
